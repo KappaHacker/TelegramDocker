811a89960cffc946bd4975415dcfe019bcac05a8
Динис Сатвалов <drkappa@mail.ru>
2021-07-09 17:28:08 +0500

    Test

---
+++ Context.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TelegramBotUser;
+
+namespace TelegramDocker
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<TelegramUser> Users { get; set; }
+
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=MigrationTelegramBot;Username=postgres;Password=CyberDude");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ TelegramUser.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TelegramBotUser
+{
+    public class TelegramUser
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public long Id { get; set; }            //айди чата 
+        public long migration { get; set; }         //значение миграции в чате с chatId
+        public bool setValueCheck { get; set; }     //состояние указывающее что нужно принять значение migration
+        public long setterValueId { get; set; } //Message в котором хранится id пользователя запустившего команду set_migration
+
+    }
+}
