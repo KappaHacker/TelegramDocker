2fe502d5dbbe442a503d79d363def15c76b8c260
KappaHacker <49730259+KappaHacker@users.noreply.github.com>
2021-07-15 10:49:03 +0500

    Add files via upload

---
+++ MigrationBot_Docker_script.ps1
@@ -0,0 +1,8 @@
+cd C:\Windows\System32\App
+dotnet restore -s -s https://www.nuget.org/api/v2/
+dotnet publish -p:Version=$"1.0" --no-restore -c Release
+docker stop core-counter || true
+docker rm core-counter || true
+docker rmi counter-image || true
+docker build -t counter-image -f Dockerfile .
+docker run -it --restart unless-stopped --name core-counter counter-image
